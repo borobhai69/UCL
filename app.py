2eb694385932e7760347208eb125bac0933e9d0f
wafi <ishmam963@gmail.com>
2020-08-09 12:22:39 -0400

    fixed redirect

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ import os
 
 # -- Initialization section --
 app = Flask(__name__)
-
+app.secret_key = '_5#z2L"F4Q8z\n\xec]/'
 user = os.environ["user"]
 pw = os.environ["pw"]
 
@@ -42,7 +42,7 @@ def index():
 
 def getpredictions():
     if request.method == "GET":
-        return render_template('predict.html', time=datetime.now())
+        return render_template('predict.html', time=datetime.now(), session = session)
     else:
         # this is storing the data from the form 
         name = request.form['name']
@@ -56,10 +56,29 @@ def getpredictions():
         lyon = request.form['lyon']
         # this is connecting to mongodb
         predictions = mongo.db.predictions
+        session["username"] = name
+        session_name = session["username"]
+        session["atalanta"] = atalanta
+        session_atalanta = session["atalanta"]
+        session["psg"] = psg
+        session_psg = session["psg"]
+        session["atletico"] = atletico
+        session_atletico = session["atletico"]
+        session["leipzig"] = leipzig
+        session_leipzig = session["leipzig"]
+        session["bayern"] = bayern
+        session_bayern = session["bayern"]
+        session["barcelona"] = barcelona
+        session_barcelona = session["barcelona"]
+        session["mancity"] = mancity
+        session_mancity = session["mancity"]
+        session["lyon"] = lyon
+        session_lyon = session["lyon"]
         # insert new data
-        predictions.insert({'name': name, 'atalanta': atalanta, 'psg': psg, 'atletico': atletico, 'leipzig': leipzig, 'bayern': bayern, 'barcelona': barcelona, 'mancity': mancity, 'lyon': lyon})
+        predictions.insert({'name': session_name, 'atalanta': session_atalanta, 'psg': session_psg, 'atletico': session_atletico, 'leipzig': session_leipzig, 'bayern': session_bayern, 'barcelona': session_barcelona, 'mancity': session_mancity, 'lyon': session_lyon})
         predictions = mongo.db.predictions
         predictview = list(predictions.find({}))
         # print(predictview)
         # return a message to the user
-        return render_template('index.html', time=datetime.now(), predictview = predictview, name = name, atalanta = atalanta, psg = psg, atletico = atletico, leipzig = leipzig, bayern = bayern, barcelona = barcelona, mancity = mancity, lyon = lyon)
+        return redirect('/')
+        return render_template('index.html', time=datetime.now(), predictview = predictview, name = session_name, atalanta = session_atalanta, psg = session_psg, atletico = session_atletico, leipzig = session_leipzig, bayern = session_bayern, barcelona = session_barcelona, mancity = session_mancity, lyon = session_lyon)
